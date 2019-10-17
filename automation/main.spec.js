6a5d14bd1d6915c75e5ceaed240b963eebeda005
Fernando Mirabile <fmirabile@solstice.com>
2019-10-17 17:00:50 -0300

    Added delays to the test

--- automation/main.spec.js
+++ automation/main.spec.js
@@ -2,12 +2,6 @@ const puppeteer = require('puppeteer');
 
 const URL = 'http://localhost:3000';
 
-function delay(timeout) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
-}
-
 describe('Pokedex Tests', () => {
   let browser = null;
   let page = null;
@@ -20,6 +14,8 @@ describe('Pokedex Tests', () => {
 
 
   beforeEach(async () => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000;
+
     browser = await puppeteer.launch({ 
       headless: false,
       defaultViewport: null,
@@ -34,18 +30,20 @@ describe('Pokedex Tests', () => {
   });
 
   it('Should find charmander', async () => {
+  
     await page.waitForSelector(selectors.pokemonSearchField);
     // Escribiendo en el input
     await page.focus(selectors.pokemonSearchField);
     await page.keyboard.type('Charmander');
+    await page.waitFor(5000);
     // Apretamos Enter 
     await page.keyboard.press('Enter');
     // Vemos el resultado
     let resultSet = await page.$$(selectors.pokemonCard);
     // Solo un charmander, por lo que se
     expect(resultSet.length).toBe(1);
-
     let resultTitle = await resultSet[0].$eval(selectors.pokemonTitle, e => e.innerText);
     expect(resultTitle).toEqual("charmander");
+    await page.waitFor(5000);
   });
 });
